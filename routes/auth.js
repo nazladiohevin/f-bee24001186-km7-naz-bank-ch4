57cebe88d0371d6a0c666f6492514ae53cd17c21
nazladiohevin <nazlahevinbusiness@gmail.com>
2024-10-29 22:15:53 +0700

    feat: jwt token at login

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { AuthController } from "../controllers/authController.js";
+
+const router = new Router();
+
+const auth = new AuthController();
+
+router.post("/login", auth.login.bind(auth));
+router.post("/register", auth.register.bind(auth));
+
+export { router };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import { router as users } from "./user.js";
 import { router as accounts } from "./account.js"
 import { router as transactions } from "./transaction.js"
+import { router as auth } from "./auth.js"
 
 export const configureRoutes = (app) => {
   app.use('/api/v1/users', users);
   app.use('/api/v1/accounts', accounts);
   app.use('/api/v1/transactions', transactions);
+  app.use('/api/v1/auth', auth);
 };
